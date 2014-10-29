b8d68981b39bd2eb7a69fd995f201a0ecd6e6e67
cetinkaya <cetinkayaahmet@yahoo.com>
2014-10-29 13:37:56 +0900

    Add __init__.py

---
+++ __init__.py
@@ -0,0 +1,58 @@
+# Copyright 2014 Ahmet Cetinkaya
+
+# This file is part of pastefromhtml.
+# pastefromhtml is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+# pastefromhtml is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+# You should have received a copy of the GNU General Public License
+# along with pastefromhtml. If not, see <http://www.gnu.org/licenses/>.
+
+from zim.plugins import PluginClass, extends, WindowExtension
+from zim.actions import action
+
+import gtk
+import htmlcdparser as hcdp
+
+class PasteFromHTMLPlugin(PluginClass):
+    plugin_info = {
+        "name": _("Paste from HTML"),
+        "description": _("This plugin lets you paste HTML clipboard data."),
+        "author": "Ahmet Cetinkaya",
+        "help": "Plugins:Paste from HTML"
+    }
+    
+@extends('MainWindow')
+class MainWindowExtension(WindowExtension):
+    uimanager_xml = '''
+    <ui>
+    <menubar name='menubar'>
+    <menu action='tools_menu'>
+    <placeholder name='plugin_items'>
+    <menuitem action='pastefh'/>
+    </placeholder>
+    </menu>
+    </menubar>
+    </ui>
+    '''
+
+    def get_clipboard_data(self):
+        my_targets = ["text/html", "utf8-string", "text", "string"]
+        clipboard = gtk.Clipboard()
+        targets = clipboard.wait_for_targets()
+        for my_target in my_targets:
+            if my_target in targets:
+                return clipboard.wait_for_contents(my_target).data.decode("utf_16").replace("\x00", "")
+            elif my_target.upper() in targets:
+                return clipboard.wait_for_contents(my_target.upper()).data
+        return ""
+    
+    @action(_('_Paste from HTML'), accelerator="<ctrl><shift>v")
+    def pastefh(self):
+        buffer = self.window.pageview.view.get_buffer()
+        h = hcdp.HTMLCDParser()
+        buffer.insert_at_cursor(h.to_zim(self.get_clipboard_data()))
